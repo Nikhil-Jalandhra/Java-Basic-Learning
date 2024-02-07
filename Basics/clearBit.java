653133f0a6ce3fae50b8781c2826045b473125fc
Nikhil-Jalandhra <jneek36@gmail.com>
2024-02-07 22:02:54 +0530

    ClearBit, Learning about bit

---
+++ Basics/clearBit.java
@@ -0,0 +1,17 @@
+public class clearBit {
+    public static void main(String[] args) {
+
+        // To clear bit from specific position
+
+        int checkNumber = 5;
+        int checkPosition = 0;
+        int bitMask = 1<<checkPosition;
+
+        int notBitMask = ~(bitMask);
+
+        int newNumber = notBitMask & checkNumber;
+
+        System.out.println("After clearing the number = " + newNumber);
+    }
+
+}
