863979e165d47380eb2c30859ed2bfdf4e62b8f6
Nikhil-Jalandhra <jneek36@gmail.com>
2024-02-08 22:25:46 +0530

    GetBit, Learning about bit

---
+++ Basics/getBit.java
@@ -0,0 +1,17 @@
+public class getBit {
+    public static void main(String[] args) {
+
+        //  To check the Bit of Number is zero or one.
+
+        int checkNumber = 5;
+        int checkPosition = 3;
+
+        int bitMask = 1<<checkPosition;
+
+        if ((bitMask & checkNumber) == 0) {
+            System.out.println("The Number is 0");
+        }else{
+            System.out.println("The Number is 1");
+        }
+    }
+}
