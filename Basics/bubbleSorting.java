66a906f8af18118853b2d0f809efeefeb132a0c0
Nikhil-Jalandhra <jneek36@gmail.com>
2024-02-09 20:39:56 +0530

    bubbleSort, Learning About Sorting

---
+++ Basics/bubbleSorting.java
@@ -0,0 +1,26 @@
+public class bubbleSorting {
+    public static void printArray(int myarr[]){
+        for (int i = 0; i < myarr.length; i++) {
+            System.err.print(myarr[i] + " ");
+        }
+    }
+    
+    public static void main(String[] args) {
+
+        // First Sorting in Java (Bubble sort)
+
+        int myarr[] = {8, 5, 7, 1, 4, 3, 2, 9, 6};
+
+        for (int i = 0; i < myarr.length-1 ; i++) {
+            for (int j = 0; j < myarr.length-1-i; j++) {
+                if (myarr[j] > myarr[j+1]) {
+                    int temp = myarr[j];
+                    myarr[j] =  myarr[j+1];
+                    myarr[j+1] = temp;
+
+                }
+            }
+        }
+        printArray(myarr);
+    }
+}
