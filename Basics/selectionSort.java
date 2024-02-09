4ccd0dfa12ebb232728badd47bd8a01b85ad85ee
Nikhil-Jalandhra <jneek36@gmail.com>
2024-02-09 20:39:07 +0530

    SelectionSort, Learning About Sorting

---
+++ Basics/selectionSort.java
@@ -0,0 +1,27 @@
+public class selectionSort {
+    public static void printArray(int myarr[]){
+        for (int i = 0; i < myarr.length; i++) {
+            System.err.print(myarr[i] + " ");
+        }
+    }
+    
+    public static void main(String[] args) {
+
+        // Selection Sort
+
+        int myarr[] = {8, 5, 7, 1, 4, 3, 2, 9, 6};
+
+        for (int i = 0; i < myarr.length-1 ; i++) {
+            int samllest = i;
+            for (int j = i+1; j < myarr.length; j++) {
+                if (myarr[samllest] > myarr[j]) {
+                    samllest = j;
+                }
+            }
+            int temp = myarr[samllest];
+            myarr[samllest] = myarr[i];
+            myarr[i] = temp; 
+        }
+        printArray(myarr);
+    }
+}
