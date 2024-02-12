5dbfb73c195cd2d7f036aa4d4140d334f9563e4c
Nikhil-Jalandhra <jneek36@gmail.com>
2024-02-12 22:31:17 +0530

    InsertionSort, Learning About Sorting

---
+++ Basics/insetrionSort.java
@@ -0,0 +1,25 @@
+public class insetrionSort {
+    public static void printArray(int myarr[]){
+        for (int i = 0; i < myarr.length; i++) {
+            System.err.print(myarr[i] + " ");
+        }
+    }
+    
+    public static void main(String[] args) {
+
+        // Insertion Sort
+
+        int myarr[] = {8, 5, 7, 1, 4, 3, 2, 9, 6};
+
+        for (int i = 0; i < myarr.length-1 ; i++) {
+            int current = myarr[i];
+            int j = i-1;
+            while (j >= 0 && current < myarr[j]) {
+                myarr[j+1] = myarr[j];
+                j--;
+            }
+            myarr[j+1] = current;
+        }
+        printArray(myarr);
+    }
+}
