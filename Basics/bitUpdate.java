7e38b901316c7574512b3b1a6296f107e3ab91b6
Nikhil-Jalandhra <jneek36@gmail.com>
2025-01-08 20:18:41 +0530

    learning update bit concept or revising

--- Basics/bitUpdate.java
+++ Basics/bitUpdate.java
@@ -1,19 +1,24 @@
+import java.util.*;
+
 public class bitUpdate {
     public static void main(String[] args) {
-        
-        // To update bit from specific position
+        Scanner sc = new Scanner(System.in);
+        System.err.println("Enter 0/1 to convert bit");
+        int query = sc.nextInt();
 
         int checkNumber = 5;
-        // 0101
-        // 0100
-        // 1011
-        int checkPosition = 2;
-
-        int bitMask = 1<<checkPosition;
-
-        int newNumber = checkNumber | bitMask;
-
-        System.out.println("Your new number is: " + newNumber);
+        int bitPosition = 1;
+        int bitMask = 1<<bitPosition;
+        
+        if (query == 1) {
+            int newNumberWith1 = bitMask | checkNumber;
+            System.out.println("Your new number is: " + newNumberWith1);
+        }else if (query == 0) {
+            int notBitMask = ~(bitPosition);
+            int newNumberWith0 = notBitMask & checkNumber;
+            System.out.println("Your new number is: " + newNumberWith0);
+        }
+        sc.close();
 
     }
 }
