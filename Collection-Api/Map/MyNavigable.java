453047c195c0a54fa0d154bbbb5e65afc98136f5
Nikhil-Jalandhra <jneek36@gmail.com>
2025-03-20 08:49:46 +0530

    studing collection api and map

---
+++ Collection-Api/Map/MyNavigable.java
@@ -0,0 +1,7 @@
+package Map;
+
+public class MyNavigable {
+    public static void main(String[] args) {
+        // It extend sorted map
+    }
+}
--- Collection-Api/Map/MySortedMap.java
+++ Collection-Api/Map/MySortedMap.java
@@ -1,5 +1,23 @@
 package Map;
 
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 public class MySortedMap {
-    
+    public static void main(String[] args) {
+        // sorted array is a interface that implement tree
+        // entries are sorted based on the keys, either in the natural order or based on comparator.
+        // 
+        SortedMap<Integer, String> loan = new TreeMap<>((a,b) -> b-a);
+        loan.put(89, "Vivek");
+        loan.put(95, "Sammer");
+        loan.put(99, "Rahul");
+        loan.put(75, "Anmol");
+
+        System.out.println(loan);
+        System.out.println(loan.firstKey());
+        System.out.println(loan.lastKey());
+        System.out.println(loan.headMap(95));
+        System.out.println(loan.tailMap(95));
+    }
 }
